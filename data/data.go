4364f014fab846e9dd16d4127c516acbbeb7a2db
Dan Kochetov <dan@lambda.direct>
2021-06-13 14:12:03 +0300

    Implement optimized dynamic stats calculation

--- data/data.go
+++ data/data.go
@@ -11,29 +11,36 @@ type Set []Point
 
 type Stats struct {
 	Average float64 `json:"average"`
+	Sum     float64
 	High    float64 `json:"high"`
 	Low     float64 `json:"low"`
 	Open    float64 `json:"open"`
 	Close   float64 `json:"close"`
+	Count   int
 }
 
 func (s Stats) MergeWith(s2 Stats) *Stats {
+	count := s.Count + s2.Count
+	sum := s.Sum + s2.Sum
 	return &Stats{
 		Open:    s2.Open,
 		Close:   s.Close,
 		High:    math.Max(s.High, s2.High),
 		Low:     math.Min(s.Low, s2.Low),
-		Average: (s.Average + s2.Average) / 2.0,
+		Average: sum / float64(count),
+		Sum:     sum,
+		Count:   count,
 	}
 }
 
 func (s Set) SliceByTime(time uint64) Set {
-	for i, p := range s {
-		if p.Timestamp < time {
-			return s[:i]
-		}
+	initialTimestamp := s[0].Timestamp
+	from := 0
+	to := (initialTimestamp-time)/10 + 1
+	if to > uint64(len(s)) {
+		to = uint64(len(s))
 	}
-	return s
+	return s[from:to]
 }
 
 func (s Set) CalcStats() *Stats {
@@ -41,13 +48,13 @@ func (s Set) CalcStats() *Stats {
 		Low:   math.MaxFloat64,
 		Open:  s[len(s)-1].Rate,
 		Close: s[0].Rate,
+		Count: len(s),
 	}
 	for _, p := range s {
 		res.Low = math.Min(res.Low, p.Rate)
 		res.High = math.Max(res.High, p.Rate)
-		res.Average += p.Rate
+		res.Sum += p.Rate
 	}
-	res.Average /= float64(len(s))
 
 	return &res
 }
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"context"
-	"github.com/lambda-direct/gocast-stock-calc/pg"
-	"github.com/lambda-direct/gocast-stock-calc/util"
+	"encoding/binary"
+	"io"
+	"io/ioutil"
 	"log"
 	"math"
 	"os"
@@ -12,38 +12,60 @@ import (
 
 	"github.com/lambda-direct/gocast-stock-calc/data"
 	"github.com/lambda-direct/gocast-stock-calc/time"
+	"github.com/lambda-direct/gocast-stock-calc/util"
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	allStart := gotime.Now()
 
-	pgClient, err := pg.New(ctx)
+	f, err := os.Open("data.bin")
 	util.PanicOnError(err)
 
-	defer pgClient.Close()
-
-	file, err := os.Open("data.json")
+	ds, err := readDataSet(f)
 	util.PanicOnError(err)
 
-	ds, err := pgClient.SyncData(ctx, file)
-	util.PanicOnError(err)
+	log.Printf("parse took %dms", gotime.Since(allStart).Milliseconds())
 
 	initialTime := ds[0].Timestamp
+
+	start := gotime.Now()
 
 	ds5min := ds.SliceByTime(initialTime - time.New(5, gotime.Minute).To(gotime.Millisecond))
 	ds30min := ds.SliceByTime(initialTime - time.New(30, gotime.Minute).To(gotime.Millisecond))
 	ds4h := ds.SliceByTime(initialTime - time.New(4, gotime.Hour).To(gotime.Millisecond))
 	ds24h := ds.SliceByTime(initialTime - time.New(24, gotime.Hour).To(gotime.Millisecond))
 
-	now := gotime.Now()
-	recursion(ds5min)
-	recursion(ds30min)
-	recursion(ds4h)
-	recursion(ds24h)
-	log.Printf("consecutive took %.3fms", float64(gotime.Since(now).Nanoseconds())/float64(gotime.Millisecond))
+	//now := gotime.Now()
+	//recursion(ds5min)
+	//recursion(ds30min)
+	//recursion(ds4h)
+	//recursion(ds24h)
+	//log.Printf("consecutive took %.3fms", float64(gotime.Since(now).Nanoseconds())/float64(gotime.Millisecond))
 
-	now = gotime.Now()
+	//var wg sync.WaitGroup
+	//wg.Add(4)
+	//
+	//go func() *data.Stats {
+	//	defer wg.Done()
+	//	return recursion(ds5min)
+	//}()
+	//
+	//go func() *data.Stats {
+	//	defer wg.Done()
+	//	return recursion(ds30min)
+	//}()
+	//
+	//go func() *data.Stats {
+	//	defer wg.Done()
+	//	return recursion(ds4h)
+	//}()
+	//
+	//go func() *data.Stats {
+	//	defer wg.Done()
+	//	return recursion(ds24h)
+	//}()
+	//
+	//wg.Wait()
 
 	tupleChan := parallel(
 		[]*TupleInput{
@@ -78,11 +100,29 @@ func main() {
 		log.Printf("%s %.3fms %+v", tuple.name, tuple.durationMs, tuple.stats)
 	}
 
-	log.Printf("parallel took %.3fms", float64(gotime.Since(now).Nanoseconds())/float64(gotime.Millisecond))
+	log.Printf("parallel took %.3fms", float64(gotime.Since(start).Nanoseconds())/float64(gotime.Millisecond))
+
+	start = gotime.Now()
+
+	res := dynamic(ds, []uint64{
+		initialTime - time.New(5, gotime.Minute).To(gotime.Millisecond),
+		initialTime - time.New(30, gotime.Minute).To(gotime.Millisecond),
+		initialTime - time.New(4, gotime.Hour).To(gotime.Millisecond),
+		initialTime - time.New(24, gotime.Hour).To(gotime.Millisecond),
+	}, initialTime)
+
+	log.Printf("dynamic took %dms", gotime.Since(start).Milliseconds())
+
+	log.Println(gotime.Unix(int64(initialTime/1000), int64(gotime.Duration(initialTime%1000)*gotime.Millisecond)).Format(gotime.RFC1123))
+	for ts, stats := range res {
+		log.Printf("ts=%s stats=%#v", gotime.Unix(int64(ts/1000), int64(gotime.Duration(ts%1000)*gotime.Millisecond)).Format(gotime.RFC1123), stats)
+	}
+
+	log.Printf("all took %dms", gotime.Since(allStart).Milliseconds())
 }
 
 func recursion(ds data.Set) *data.Stats {
-	if len(ds) <= int(math.Pow(2, 13)) {
+	if len(ds) <= 4000 {
 		return ds.CalcStats()
 	}
 
@@ -141,4 +181,101 @@ func parallel(tuples []*TupleInput) <-chan *TupleOutput {
 	}()
 
 	return tupleChan
+}
+
+func dynamic(ds data.Set, timestamps []uint64, initialTimestamp uint64) map[uint64]*data.Stats {
+	res := make(map[uint64]*data.Stats, len(timestamps))
+
+	dsChunks := make([]data.Set, len(timestamps))
+
+	prevTimestamp := initialTimestamp
+	for i, ts := range timestamps {
+		from := (initialTimestamp - prevTimestamp) / 10
+		to := (initialTimestamp-ts)/10 + 1
+		if to > uint64(len(ds)) {
+			to = uint64(len(ds))
+		}
+		dsChunks[i] = ds[from:to]
+
+		prevTimestamp = ts
+	}
+
+	for i, chunk := range dsChunks {
+		s := calcStats(chunk)
+		if i > 0 {
+			s = res[timestamps[i-1]].MergeWith(*s)
+		}
+		res[timestamps[i]] = s
+	}
+
+	return res
+}
+
+func calcStats(ds data.Set) *data.Stats {
+	const DataChunkSize = 2000
+	ChunkAmount := int(math.Ceil(float64(len(ds)) / DataChunkSize))
+
+	statsList := make([]*data.Stats, ChunkAmount)
+
+	var wg sync.WaitGroup
+	wg.Add(ChunkAmount)
+
+	for i := 0; i < ChunkAmount; i++ {
+		go func(i int) {
+			defer wg.Done()
+			from := i * DataChunkSize
+			to := int(math.Min(float64((i+1)*DataChunkSize), float64(len(ds))))
+			statsList[i] = ds[from:to].CalcStats()
+		}(i)
+	}
+
+	wg.Wait()
+
+	res := statsList[0]
+	for _, s := range statsList[1:] {
+		res = res.MergeWith(*s)
+	}
+
+	return res
+}
+
+func parsePoint(b []byte) data.Point {
+	ts := binary.LittleEndian.Uint64(b[:8])
+	rateUint64 := binary.LittleEndian.Uint64(b[8:16])
+	rate := math.Float64frombits(rateUint64)
+	return data.Point{
+		Timestamp: ts,
+		Rate:      rate,
+	}
+}
+
+func readDataSet(f io.Reader) (data.Set, error) {
+	const GoroutinesAmount = 100
+	const DataSetSize = 8_640_000
+	const DataChunkSize = DataSetSize / GoroutinesAmount
+
+	bytes, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	ds := make(data.Set, DataSetSize)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < GoroutinesAmount; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+
+			for j := 0; j < DataChunkSize; j++ {
+				offset := i*DataChunkSize + j*16
+				ds[i*DataChunkSize+j] = parsePoint(bytes[offset : offset+16])
+			}
+		}(i)
+	}
+
+	wg.Wait()
+
+	return ds, nil
 }
